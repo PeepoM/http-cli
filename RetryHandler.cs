a63ab18bb082745880519763b4cbb6f0ae027c01
PeepoM <peter.marks@outlook.sk>
2022-06-10 12:34:06 +0200

    implemented "retry handler" for repetitive request sending when conditions are met

---
+++ RetryHandler.cs
@@ -0,0 +1,38 @@
+namespace project;
+
+internal class RetryHandler : DelegatingHandler
+{
+    private readonly int _maxRetries;
+    private readonly int _waitTimeMillis;
+
+    public RetryHandler(int maxRetries = 3, int waitTimeMillis = 3000) : base(new HttpClientHandler())
+    {
+        _maxRetries = maxRetries;
+        _waitTimeMillis = waitTimeMillis;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        HttpResponseMessage response;
+        bool isIdempotent = request.Method != HttpMethod.Post &&
+                            request.Method != HttpMethod.Patch;
+
+        int i = 0, statusCode;
+        do
+        {
+            response = await base.SendAsync(request, cancellationToken);
+
+            statusCode = (int)response.StatusCode;
+
+            if ((statusCode is >= 500 and < 600) && isIdempotent && i + 1 < _maxRetries)
+            {
+                Console.WriteLine($"Waiting {_waitTimeMillis / 1000} sec before trying again...");
+                Thread.Sleep(_waitTimeMillis);
+            }
+        } while ((statusCode is >= 500 and < 600) && isIdempotent && i++ < _maxRetries);
+
+        return response;
+    }
+}
